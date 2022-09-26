fd884b3f06b1a9dc21eee98e64b44ee6dccb4474
Florjan96 <florianshiqerukaj@yahoo.com>
2022-09-26 13:04:27 +0200

    Create state ,done. Fetching(useEffect),done. Map, done.

--- src/App.tsx
+++ src/App.tsx
@@ -35,6 +35,7 @@ function App() {
             </div>
             <ul className="comments">
               <li>Get rid of these comments</li>
+              <li>Get rid of these comments</li>
               <li>And replace them with the real ones</li>
               <li>From the server</li>
             </ul>
